46bf9e4c049c50ce538679f95eafc9dd96c29612
Denis Volkov <d.g.volkovbz@gmail.com>
2021-12-13 22:10:23 +0400

    add adaptive mobile menu and burger

--- app/js/main.js
+++ app/js/main.js
@@ -21,4 +21,14 @@ $(function () {
     anchors: ['top', 'product', 'benefits', 'specification', 'questions', 'contacts'],
   });
 
-})
+  $('.menu__btn').on('click', function () {
+    $('.menu__btn').toggleClass('menu__btn--active');
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
+  $('.menu__list-link').on('click', function () {
+    $('.menu__btn').removeClass('menu__btn--active');
+    $('.menu__list').removeClass('menu__list--active');
+  });
+
+});
